a1102da8b9d2804a7aa5ffc0af1bf5a26cac3711
JoMobile <lapinette2002@ymail.com>
2023-02-16 09:55:58 +0100

    Ajout de la fonctionnalité d'ajout de link

--- src/Controller/LinkController.php
+++ src/Controller/LinkController.php
@@ -2,13 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Link;
 use App\Repository\LinkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class LinkController extends AbstractController
 {
+    #[Route('/link/add', name: 'link_add')]
+    public function add(Request $request, LinkRepository $linkRepository): Response
+    {
+        $link = new Link();
+
+        $form = $this->createFormBuilder($link)
+            ->add('URL', TextType::class)
+            ->add('title', TextType::class)
+            ->add('save', SubmitType::class, ["label" => "Ajouter un lien"])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$link` variable has also been updated
+            $link = $form->getData();
+
+            // ... perform some action, such as saving the link to the database
+
+            $linkRepository->save($link, true);
+
+            return $this->redirectToRoute('home');
+        }
+
+
+        return $this->render('link/add.html.twig', [
+            'controller_name' => 'LinkController',
+            'form' => $form->createView()
+        ]);
+    }
+
     #[Route('/link', name: 'link')]
     public function index(): Response
     {
